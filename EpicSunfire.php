0c24a256afb7d9df2ccedc4311d268e59620620b
sniGG <sniggw5@yahoo.de>
2016-01-24 15:45:51 +0100

    small fixes

--- EpicSunfire.php
+++ EpicSunfire.php
@@ -22,7 +22,7 @@ class EpicSunfire {
 
 		$originals = [
 						"o" => [], 
-						"o2" => ["ans", "com", "cwd", "rpz", "san", "smn"], //"ais", "cid", "csd", "vti", "vti2", "xxx" ;
+						"o2" => ["ans", "com", "cwd", "rpz", "san", "smn", "xxx"], //"ais", "cid", "csd", "vti", "vti2" ;
 						"o3" => [], 
 						"o4" => []
 					 ];
